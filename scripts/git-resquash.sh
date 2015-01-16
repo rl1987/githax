ade2470d12d3f1bd5aa3f908bd2290f565f28abf
Nick Mathewson <nickm@torproject.org>
2015-01-16 09:36:44 -0500

    Bulletproof my git-resquash.sh script a little

--- scripts/git-resquash.sh
+++ scripts/git-resquash.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-REV=`git log --reverse --format='%H' master..HEAD | head -1`
+PARENT="$1"
+
+if test "x$PARENT" = "x"; then
+    echo "You must specify the parent branch."
+    exit 1
+fi
+
+REV=`git log --reverse --format='%H' ${PARENT}..HEAD | head -1`
+
+if test "x${REV}" = "x"; then
+    echo "No changes here since ${PARENT}"
+    exit 1
+fi
 
 git rebase -i --autosquash ${REV}^
 
